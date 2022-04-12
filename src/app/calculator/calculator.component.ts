343972cefe98d4ef8c459f439c404a73679fdf8f
Ravikumar Rao <ravikumar.rao@thomsonreuters.com>
2022-04-12 18:21:46 -0400

    Styling cleanup + keypress restrictions

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -73,4 +73,13 @@ export class CalculatorComponent implements OnInit {
     this._calcService.calculateNetworth(this.user)
       .subscribe(b => this.balances = b);
   }
+
+  numberOnly(event: KeyboardEvent): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
 }
